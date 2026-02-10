f5373a271d51c29c6e3852a8d1349ceb6ad7f587
Alexey Vorobev <vorobalek@icloud.com>
2026-02-10 18:58:14 +0400

    Changed variable name from `$path` to `$mindbox_path`

--- mindbox_integration/include.php
+++ mindbox_integration/include.php
@@ -1,8 +1,8 @@
 <?php
 
-$path = __DIR__ . '/bootstrap.php';
-if (is_file($path)) {
-    require_once $path;
+$mindbox_path = __DIR__ . '/bootstrap.php';
+if (is_file($mindbox_path)) {
+    require_once $mindbox_path;
 }
 
 if (class_exists('MindboxCustomerOperations', false) && method_exists('MindboxCustomerOperations', 'registerHandlers')) {
@@ -16,9 +16,9 @@ if (class_exists('MindboxCustomerOperations', false) && method_exists('MindboxCu
 if (!function_exists('mindbox_integration_agent')) {
     function mindbox_integration_agent()
     {
-        $path = __DIR__ . '/bootstrap.php';
-        if (is_file($path)) {
-            require_once $path;
+        $mindbox_path = __DIR__ . '/bootstrap.php';
+        if (is_file($mindbox_path)) {
+            require_once $mindbox_path;
         }
 
         if (class_exists('MindboxIntegrationQueueService', false)) {
